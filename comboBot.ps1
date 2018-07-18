f55ad7ef105db66442386819514d10b1ff39a656
ppotter50 <ppotter50@yahoo.com>
2018-07-18 14:00:49 -0400

    tusting

--- comboBot.ps1
+++ comboBot.ps1
@@ -404,7 +404,7 @@ while ($infinite) {
 						$histob = $hist.Content | ConvertFrom-Json
 
 						$loadlist = loadList
-						$histob.messages.attachments.text | Out-File -FilePath 'c:\Users\Paul Potter\Downloads\DeleteThis\botlog.txt' #for testing and debugging
+						$loads | Out-File -FilePath 'c:\Users\Paul Potter\Downloads\DeleteThis\botlog.txt' #for testing and debugging
 
 						$loadencode = [System.Web.HttpUtility]::UrlEncode("The following computers were successfully loaded on $(@($SplitMatches)[0])")
 						Invoke-WebRequest -Uri "https://slack.com/api/chat.postMessage?token=$token&channel=$paulstesting&text=$loadencode&attachments=[{`'color`':`'$purple`',`'text`':`'$loadlist`'}]" -Method 'POST'
